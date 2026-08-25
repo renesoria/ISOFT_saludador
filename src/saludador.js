038cc0884f732353d42743a37cf7036c99bbda40
Soria <rene.soria@ucb.edu.bo>
2026-08-24 23:45:16 -0400

    feat: saludar de acuerdo a la hora

--- src/saludador.js
+++ src/saludador.js
@@ -1,4 +1,12 @@
 function saludar(nombre){
-    return "Hola " + nombre;
+    let fechaActual=new Date();
+    let horaActual=fechaActual.getHours();
+    if(horaActual<12){
+        return "Buenos días " + nombre;
+    }else if(horaActual<18){
+        return "Buenas tardes " + nombre;
+    }else{
+        return "Buenas noches " + nombre;
+    }
 }
 export default saludar;
