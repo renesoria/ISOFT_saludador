63a904715158d7e16b2e6d74611ab4c1d90f07ff
Soria <rene.soria@ucb.edu.bo>
2026-08-24 23:53:36 -0400

    feat:saludar por genero

--- src/presenter.js
+++ src/presenter.js
@@ -2,10 +2,11 @@ import saludar from "./saludador";
 
 const form = document.querySelector("#saludar-form");
 const nombre= document.querySelector("#nombre-input");
+const genero = document.querySelector("#genero");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  div.innerHTML = "<p>" + saludar(nombre.value) + "</p>";
+  div.innerHTML = "<p>" + saludar(nombre.value,genero.value) + "</p>";
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,12 +1,22 @@
-function saludar(nombre){
-    let fechaActual=new Date();
-    let horaActual=fechaActual.getHours();
-    if(horaActual<12){
-        return "Buenos días " + nombre;
-    }else if(horaActual<18){
-        return "Buenas tardes " + nombre;
-    }else{
-        return "Buenas noches " + nombre;
+function saludar(nombre, genero) {
+    let fechaActual = new Date();
+    let horaActual = fechaActual.getHours();
+
+    let saludo;
+
+    if (horaActual < 12) {
+        saludo = "Buenos días";
+    } else if (horaActual < 18) {
+        saludo = "Buenas tardes";
+    } else {
+        saludo = "Buenas noches";
     }
+
+    if (genero === "femenino") {
+        return saludo + " " + nombre + ", bienvenida";
+    }
+
+    return saludo + " " + nombre + ", bienvenido";
 }
+
 export default saludar;
